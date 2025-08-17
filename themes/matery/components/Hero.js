1eec7e2272a61f50b488208785d422cda4254ecf
Layla Joines <80381650+SudieSzoka@users.noreply.github.com>
2025-08-17 08:12:15 +0800

    Revert "Merge branch 'main' of https://github.com/SudieSzoka/china144h"
    This reverts commit 4c095027a239c7897f045252310b22baab40d235, reversing
    changes made to 538b7daf26109fa6e95fb5e2390ad319d2c202bc.

--- themes/matery/components/Hero.js
+++ themes/matery/components/Hero.js
@@ -54,7 +54,7 @@ const Hero = props => {
     <header
       id='header'
       style={{ zIndex: 1 }}
-      className=' w-full relative bg-black'>
+      className=' w-full h-screen relative bg-black'>
       <div className='text-white absolute flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
         <div className='text-4xl md:text-5xl shadow-text'>
@@ -64,18 +64,26 @@ const Hero = props => {
         <div className='mt-2 h-12 items-center text-center shadow-text text-white text-lg'>
           <span id='typed' />
         </div>
-
+        {/* 滚动按钮 */}
+        <div
+          onClick={() => {
+            window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
+          }}
+          className='glassmorphism mt-12 border cursor-pointer w-40 text-center pt-4 pb-3 text-md text-white hover:bg-orange-600 duration-300 rounded-3xl z-40'>
+          <i className='animate-bounce fas fa-angle-double-down' />{' '}
+          <span>
+            {siteConfig('MATERY_SHOW_START_READING', null, CONFIG) &&
+              locale.COMMON.START_READING}
+          </span>
+        </div>
       </div>
 
-      {/* 修改这里的样式，调整高度为20% */}
-      <div style={{ height: '20vh' }} className='relative'>
-        <LazyImage
-          priority={true}
-          id='header-cover'
-          src={siteInfo?.pageCover}
-          className={`header-cover object-center w-full h-full object-cover ${siteConfig('MATERY_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
-        />
-      </div>
+      <LazyImage
+        priority={true}
+        id='header-cover'
+        src={siteInfo?.pageCover}
+        className={`header-cover object-center w-full h-screen object-cover ${siteConfig('MATERY_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
+      />
     </header>
   )
 }
